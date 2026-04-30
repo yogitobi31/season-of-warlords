1d46dfdbb59d6ffb3f8deb62e41fde6adbe7e507
yogitobi31 <jeonant@naver.com>
2026-05-01 02:48:16 +0900

    Improve battle result UX and world map occupation feedback

--- scripts/GameState.gd
+++ scripts/GameState.gd
@@ -27,6 +27,7 @@ var selected_region_id: String = ""
 var attack_region_id: String = ""
 var defense_region_id: String = ""
 var last_battle_result: String = ""
+var last_battle_message: String = ""
 
 func _ready() -> void:
 	# 게임 시작 시 지역 데이터를 초기화합니다.
@@ -40,6 +41,7 @@ func initialize_regions() -> void:
 		ownership[region_id] = regions[region_id]["owner"]
 	clear_selection()
 	last_battle_result = ""
+	last_battle_message = ""
 
 func clear_selection() -> void:
 	selected_region_id = ""
@@ -64,10 +66,22 @@ func set_battle_context(attacker_region_id: String, defender_region_id: String) 
 	defense_region_id = defender_region_id
 
 func apply_battle_result(player_won: bool) -> void:
-	# 플레이어 승리 시 방어 지역 소유권이 플레이어로 변경됩니다.
-	if player_won and defense_region_id != "":
-		set_region_owner(defense_region_id, PLAYER_FACTION)
+	var target_region_name := ""
+	if defense_region_id != "" and regions.has(defense_region_id):
+		target_region_name = str(regions[defense_region_id].get("name", "미지의 지역"))
+
+	if player_won:
+		# 플레이어 승리 시 방어 지역 소유권이 플레이어로 변경됩니다.
+		if defense_region_id != "":
+			set_region_owner(defense_region_id, PLAYER_FACTION)
 		last_battle_result = "player_win"
+		last_battle_message = "승리! %s를 점령했습니다." % target_region_name
 	else:
 		last_battle_result = "player_lose"
+		if attack_region_id != "" and regions.has(attack_region_id):
+			var retreat_region_name := str(regions[attack_region_id].get("name", "아군 지역"))
+			last_battle_message = "패배... %s로 후퇴합니다." % retreat_region_name
+		else:
+			last_battle_message = "패배... 아군 지역으로 후퇴합니다."
+
 	clear_selection()
